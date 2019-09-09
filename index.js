22f548553edc9f65c093dd1ec18f172c302f58fe
Leonardo Souza <leonardo.souza@trigg.com>
2019-09-09 13:15:00 -0300

    cathing errros

--- index.js
+++ index.js
@@ -7,15 +7,31 @@ const base_url_img = 'https://poring.world/sprites/';
 
 (() => {
   const item = process.argv[2];
+
+  if (!item) {
+    growl.icon('error').notify('No item received');
+    return 1;
+  }
+
   axios
     .get(base_url_api, {
       params: { order: 'popularity', inStock: 1, q: item.toLowerCase() }
     })
     .then(({ data }) => {
+      if (!data || !data[0]) {
+        growl.icon('error').notify('No data received');
+        return 1;
+      }
       data = data[0];
       let value = 'Ƶ ' + Number(data.lastRecord.price).toLocaleString();
-      if (data.lastRecord.snapEnd > 0 && new Date(data.lastRecord.snapEnd*1000).getTime() > new Date().getTime()) {
-        value += `\n<i>(in snap until ${new Date(data.lastRecord.snapEnd*1000).toLocaleString()})</i>`
+      if (
+        data.lastRecord.snapEnd > 0 &&
+        new Date(data.lastRecord.snapEnd * 1000).getTime() >
+          new Date().getTime()
+      ) {
+        value += `\n<i>(in snap until ${new Date(
+          data.lastRecord.snapEnd * 1000
+        ).toLocaleString()})</i>`;
       }
       const img = `${base_url_img}${data.icon}.png`;
 
