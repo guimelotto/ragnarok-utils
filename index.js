10a0007ef00fa51add7b3b414dd5db7d7aebae13
Leonardo Souza <leonardo.souza@trigg.com>
2019-09-10 08:14:35 -0300

    Refactory (separando funçoes)

--- index.js
+++ index.js
@@ -1,67 +1,46 @@
+'use strict';
+
 const growl = require('notify-send');
-const axios = require('axios');
-const fs = require('fs');
+const { fetchItem, downloadImage } = require('./lib/functions.js')
 
-const base_url_api = 'https://poring.world/api/search';
-const base_url_img = 'https://poring.world/sprites/';
+const item = process.argv[2];
 
-(() => {
-  const item = process.argv[2];
+if (!item) {
+  growl.icon('error').notify('No item received');
+  return 1;
+}
 
-  if (!item) {
-    growl.icon('error').notify('No item received');
+fetchItem(item).then(async r => {
+  if (!r.data || !r.data[0]) {
+    growl.icon('error').notify('No data received');
     return 1;
   }
 
-  axios
-    .get(base_url_api, {
-      params: { order: 'popularity', inStock: 1, q: item.toLowerCase() }
-    })
-    .then(({ data }) => {
-      if (!data || !data[0]) {
-        growl.icon('error').notify('No data received');
-        return 1;
-      }
-      data = data[0];
-      let value = 'Ƶ ' + Number(data.lastRecord.price).toLocaleString() + ` (${Math.round(data.priceChange1d)}%)`;
-      if (
-        data.lastRecord.snapEnd > 0 &&
-        new Date(data.lastRecord.snapEnd * 1000).getTime() >
-          new Date().getTime()
-      ) {
-        value += `\n<i>(in snap until ${new Date(
-          data.lastRecord.snapEnd * 1000
-        ).toLocaleString()})</i>`;
+  let data = r.data[0];
 
-        value += `\n[<b>Stock</b>: ${data.lastRecord.stock} | <b>Buyers</b>: ${data.lastRecord.snapBuyers}]`
-      }
-      const img = `${base_url_img}${data.icon}.png`;
+  let value = 'Ƶ ' + Number(data.lastRecord.price).toLocaleString() + ` (${Math.round(data.priceChange1d)}%)`;
 
-      const fileName = data.name
-        .replace(/\+\d+|\[\d+\]|\(.+\)|\<.+\>/g, ' ')
-        .trim()
-        .toLowerCase()
-        .replace(/\s+/g, '_');
+  if (
+    data.lastRecord.snapEnd > 0 &&
+    data.lastRecord.snapEnd > new Date().getTime() / 1000
+  ) {
+    value += `\n<i>(in snap until ${new Date(
+      data.lastRecord.snapEnd * 1000
+    ).toLocaleString()})</i>`;
 
-      const filePath = `${__dirname}/images/${fileName}.png`;
+    value += `\n[<b>Stock</b>: ${data.lastRecord.stock} | <b>Buyers</b>: ${data.lastRecord.snapBuyers}]`
+  }
 
-      download_image(img, filePath).then(() => {
-        console.log(`[${(new Date()).toLocaleString()}] ${data.name}: ${Number(data.lastRecord.price).toLocaleString()}`);
-        growl.icon(filePath).notify(data.name, value);
-      });
-    });
-})();
+  const fileName = data.name
+    .replace(/\+\d+|\[\d+\]|\(.+\)|\<.+\>/g, ' ')
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '_');
 
-const download_image = (url, image_path) =>
-  axios({
-    url,
-    responseType: 'stream'
-  }).then(
-    response =>
-      new Promise((resolve, reject) => {
-        response.data
-          .pipe(fs.createWriteStream(image_path))
-          .on('finish', () => resolve())
-          .on('error', e => reject(e));
-      })
-  );
+  const filePath = `${__dirname}/images/${fileName}.png`;
+
+  await downloadImage(`${data.icon}.png`, filePath);
+
+  console.log(`[${(new Date()).toLocaleString()}] ${data.name}: ${Number(data.lastRecord.price).toLocaleString()}`);
+  growl.icon(filePath).notify(data.name, value);
+});
